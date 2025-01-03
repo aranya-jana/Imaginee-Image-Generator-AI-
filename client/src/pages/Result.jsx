ca726536fd134a9c65e9b6b8517f8d84c8525640
Aranya Jana <aranyajana01@gmail.com>
2025-01-03 22:26:03 +0530

    first commit

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,34 +1,54 @@
-import React from 'react'
-import { assets } from '../assets/assets'
+import React, { useState } from 'react';
+import { assets } from '../assets/assets';
 
 const Result = () => {
+  const [image, setImage] = useState(assets.sample_img_1);
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+  const [loading, setLoading] = useState(false)
+  const [input, setInput] = useState('')
+
   return (
-    <form className='flex flex-col min-h-[90vh] justify-center items-center'>
+    <form className="flex flex-col min-h-[90vh] justify-center items-center">
       <div>
-        <div className='relative'>
-          <img src={assets.sample_img_1} alt="" className='max-w-sm rounded' />
-          <span className='absolute bottom-e left-0 h-1 bg-blue-500 w-full transition-all duration-[10s]' />
+        <div className="relative">
+          <img src={image} alt="" className="max-w-sm rounded" />
+          <span className={`absolute bottom-0 left-0 h-1 bg-blue-500 $
+            {loading ? 'w-full transition-all duration-[10s]' : 'w-0'}`} />
         </div>
-        <p className='text-white'>Imagine . . . . .</p>
+        <p className={`${!loading ? 'hidden' : ''} text-white`}>Imagine . . . . .</p>
       </div>
 
-      <div className='flex w-full max-w-xl bg-slate-800 text-sm p-0.5 mt-10 rounded-full'>
-        <input type="text" placeholder='Describe your image...' className='text-white 
-        flex-1 bg-transparent outline-none ml-8 max-sm:w-20 placeholder-color' />
-        <button type='submit' className='bg-gray-300 px-10 sm:px-16 py-3 rounded-full'>
-          Generate
-        </button>
-      </div>
-
-      <div className='flex gap-2 flex-wrap justify-center text-white text-sm p-0.5 mt-10 rounded-full'>
-        <p className='bg-transparent border border-zinc-400 px-8 py-3 rounded-full cursor-pointer'>Generate Another</p>
-        <a className='bg-gray-300 px-8 py-3 rounded-full cursor-pointer text-black' download href='' >Download</a>
-      </div>
-
+      {!isImageLoaded && 
+        <div className="flex w-full max-w-xl bg-slate-800 text-sm p-0.5 mt-10 rounded-full">
+          <input
+          onChange={e => setInput(e.target.value)} value={input}
+            type="text"
+            placeholder="Describe your image..."
+            className="text-white flex-1 bg-transparent outline-none ml-8 max-sm:w-20 placeholder-gray-500"
+          />
+          <button
+            type="submit"
+            className="bg-gray-300 px-10 sm:px-16 py-3 rounded-full"
+          >
+            Generate
+          </button>
+        </div>
+      }
+{isImageLoaded &&
+      <div className="flex gap-2 flex-wrap justify-center text-white text-sm p-0.5 mt-10 rounded-full">
+        <p onClick={() => {setIsImageLoaded(false)}} className="bg-transparent border border-zinc-400 px-8 py-3 rounded-full cursor-pointer">
+          Generate Another
+        </p>
+        <a
+          href={image}
+          download
+          className="bg-gray-300 px-8 py-3 rounded-full cursor-pointer text-black"
+        >
+          Download
+        </a>
+      </div>}
     </form>
+  );
+};
 
-
-  )
-}
-
-export default Result
+export default Result;
