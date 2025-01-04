114740edacf9af1a5494a0e7c262f28400ef9511
Aranya Jana <aranyajana01@gmail.com>
2025-01-04 22:09:33 +0530

    first commit

--- client/src/pages/BuyCredit.jsx
+++ client/src/pages/BuyCredit.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { assets, plans } from '../assets/assets'
 
 const BuyCredit = () => {
   return (
     <div className='min-h-[80vh] text-center pt-14 mb-10'>
-      <button className=' border border-gray-400 px-10 py-2 rounded-full mb-6'>Our Plans</button>
-      <h1 className='text-center text-3xl font-medium mb-6 sm:mb-10'>Upgrade your plan</h1>
+      <button className=' border border-gray-400 px-10 py-2 rounded-full mb-6 text-white'>Our Plans</button>
+      <h1 className='text-center text-3xl font-medium mb-6 sm:mb-10 text-white'>Upgrade your plan</h1>
+
+
+      <div>
+        {plans.map ((item, index)=>(
+          <div key={index}>
+            <img src={assets.logo_icon} alt="" />
+            <p>{item.id}</p>
+            <p>{item.desc}</p>
+            <p>₹{item.price} / {item.credits} credits</p>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
