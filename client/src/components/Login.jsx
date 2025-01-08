89f7a1d1acd261acd363c4d36c6197a368454fb3
Aranya Jana <aranyajana01@gmail.com>
2025-01-08 21:05:16 +0530

    first commit

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { assets } from '../assets/assets'
 
 const Login = () => {
+
+    const [state, setState] = useState('Log in')
+
   return (
     <div className='absolute top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 
     flex justify-center items-center'>
 
     <form className='relative bg-white p-10 rounded-xl text-slate-600'>
-        <h1 className='text-center text-2xl text-neutral-700 font-medium'>Sign Up</h1>
+        <h1 className='text-center text-2xl text-neutral-700 font-medium'>{state}</h1>
         <p className='text-sm'>Welcome back! Please log in to proceed</p>
 
-        <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-5'>
+        {state !== 'Log in' && <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-5'>
             <img className="custom-class" style={{ width: 25 }} src={assets.profile_icon} alt="" />
             <input type="text" className='outline-none text-sm' placeholder='Full Name' required/>
-        </div>
+        </div>}
 
         <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
             <img src={assets.email_icon} alt="" />
