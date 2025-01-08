ffe72ca72af6b6e61097c2f5ce0f2238938f7853
Aranya Jana <aranyajana01@gmail.com>
2025-01-08 19:25:44 +0530

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,11 +6,13 @@ import Result from './pages/Result'
 import BuyCredit from './pages/BuyCredit'
 import Navbar from './components/Navbar'
 import Footer from './components/footer'
+import Login from './components/Login'
 
 const App = () => {
   return (
     <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-black to-slate-800'>
       <Navbar/>
+      <Login/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/result' element={<Result/>}/>                                 
---
+++ client/src/components/Login.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Login = () => {
+  return (
+    <div className='absolute top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 
+    flex justify-center items-center'>
+
+    <form className='text-white'>
+        <h1>Sign Up</h1>
+        <p>Welcome back! Please log in to proceed</p>
+
+        <div>
+            <img src={assets.user_icon} alt="" />
+            <input type="text" placeholder='Full Name' required/>
+        </div>
+    </form>
+
+    </div>
+  )
+}
+
+export default Login
