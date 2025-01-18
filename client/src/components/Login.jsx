c146e85b0886cc8ee2ad64b5a6f7ab966a0f3e1f
Aranya Jana <aranyajana01@gmail.com>
2025-01-18 22:33:31 +0530

    first commit

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,9 +1,17 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { assets } from '../assets/assets'
 
 const Login = () => {
 
     const [state, setState] = useState('Log in')
+
+    useEffect(()=>{
+        document.body.style.overflow = 'hidden';
+
+        return ()=>{
+            document.body.style.overflow = 'unset';
+        }
+    },[])
 
   return (
     <div className='absolute top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 
