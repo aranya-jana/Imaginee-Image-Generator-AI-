5ccffefb3ac27a9360c92b013ea408d052d0f4fd
Aranya Jana <aranyajana01@gmail.com>
2025-01-20 01:18:16 +0530

    Initial commit

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -48,7 +48,7 @@ const Login = () => {
         <p className='mt-5 text-center'>Already have an account? <span className='text-green-500 
         cursor-pointer'  onClick={()=>setState('Log in')} >Log in</span></p>}
 
-        <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt=" " className='absolute top-5 right-5 cursor-pointer'/>
+        <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" className='absolute top-5 right-5 cursor-pointer'/>
 
     </form>
 
