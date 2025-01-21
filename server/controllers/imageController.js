94b76cf90c707f420ee95bc508bfb4ea39e2fe69
Aranya Jana <aranyajana01@gmail.com>
2025-01-21 13:41:15 +0530

    Initial commit

---
+++ server/controllers/imageController.js
@@ -0,0 +1,42 @@
+import axios from "axios"
+import userModel from "../models/userModel"
+import FormData from "form-data"
+
+
+
+export const generateImage = async (req, res) => {
+    try {
+        const{userId, prompt} = req.body
+        const user = await userModel.findById()
+
+        if(!user || !prompt){
+            return res .json({success: false, message: 'Missing Details'})
+        }
+
+        if(user.creditBalance === 0 || userModel.creditBalance < 0){
+            return res.json({success: false, message: 'Credit Balance 0', creditBalance: user.creditBalance})
+        }
+
+        const fromData = new FormData()
+        FormData.append('prompt', prompt)
+
+        const {data} = await axios.post('https://clipdrop-api.co/text-to-image/v1', FormData, {
+            headers: {
+                'x-api-key': process.env.CLIPDROP_API,
+              },
+              responseType: 'arraybuffer'
+        })
+
+        const based64Image = Buffer.from(data, 'binary').toString('base64')
+        const resultImage = `data: image/png, base64, ${based64Image}`
+
+        await userModel.findByIdAndUpdate(user._id, {creditBalance: user.creditBalance - 1})
+
+        res.json({success: true, message: "Image Generated", creditBalance: user.creditBalance - 1, resultImage})
+
+
+    } catch (error) {
+        console.log(error.message)
+        res.json({success: false, message: error.message})
+    }
+}
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -9,7 +9,7 @@ const registerUser = async (req, res)=> {
         const{name, email, password} = req.body;
         
         if(!name || !email || !password){
-            return res.json({sucess:false, message: 'Missing Details'})
+            return res.json({success:false, message: 'Missing Details'})
         }
 
         const salt = await bcrypt.genSalt(10)
@@ -26,11 +26,11 @@ const registerUser = async (req, res)=> {
 
         const token = jwt.sign({id: user._id}, process.env.JWT_SECRET)
 
-        res.json({sucess: true, token, user:{name: user.name}})
+        res.json({success: true, token, user:{name: user.name}})
 
     } catch (error) {
         console.log(error)
-        res.json({sucess: false, message: error.message})
+        res.json({success: false, message: error.message})
     }
 
 }
@@ -41,7 +41,7 @@ const loginUser = async (req, res)=>{
         const user = await userModel.findOne({email})
 
         if(!user){
-            return res.json({sucess:false, message: 'User does not exist'})
+            return res.json({success:false, message: 'User does not exist'})
         }
 
         const isMatch = await bcrypt.compare(password, user.password)
@@ -49,15 +49,15 @@ const loginUser = async (req, res)=>{
         if(isMatch){
             const token = jwt.sign({id: user._id}, process.env.JWT_SECRET)
 
-            res.json({sucess: true, token, user:{name: user.name}})
+            res.json({success: true, token, user:{name: user.name}})
 
         }else{
-            return res.json({sucess:false, message: 'Invalid credentials'})
+            return res.json({success:false, message: 'Invalid credentials'})
         }
 
     } catch (error) {
         console.log(error)
-        res.json({sucess: false, message: error.message})
+        res.json({success: false, message: error.message})
     }
 }
 
@@ -66,10 +66,10 @@ const userCredits= async (req, res)=>{
     try {
         const {userId} = req.body
         const user = await userModel.findById(userId)
-        res.json({sucess: true, credits: user.creditBalance, user:{name: user.name}})
+        res.json({success: true, credits: user.creditBalance, user:{name: user.name}})
     } catch (error) {
         console.log(error)
-        res.json({sucess: false, message: error.message})
+        res.json({success: false, message: error.message})
     }
 
 }
