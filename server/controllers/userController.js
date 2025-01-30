fd2dc55fbc4392d27cbc7a5ecdb1d7f9f93bc86d
Aranya Jana <aranyajana01@gmail.com>
2025-01-30 12:05:18 +0530

    Initial commit

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -146,5 +146,32 @@ const paymentRazorpay = async(req, res)=>{
     }
 }
 
+const verifyRazorpay = async (req, res)=>{
+    try {
+        
+        const {razorpay_order_id} = req.body
+        const orderInfo = await razorpayInstance.orders.fetch(razorpay_order_id)
+
+        if (orderInfo.status === 'paid') {
+            const transactionData = await transactionModel.findById(orderInfo.receipt)
+
+            if (transactionData.payment) {
+                return res.json({success: false, message: 'Payment Failed'})
+            }
+
+            const userData = await userModel.findById(transactionData.userId)
+
+            const creditBalance = userData.creditBalance + transactionData.credits
+
+            await userModel.findByIdAndUpdate(userData._id, {creditBalance})
+        }
+
+
+    } catch (error) {
+        console.log(error)
+        res.json({success: false, message: error.message})
+    }
+}
+
 
 export{registerUser, loginUser, userCredits, paymentRazorpay}
